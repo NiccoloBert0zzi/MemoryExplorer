e2af3afccb0cc3e07a4b73aa900244ef5849457c
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2024-04-21 22:30:11 +0200

    Added memory card

--- app/src/main/java/com/example/memoryexplorer/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/memoryexplorer/ui/screens/home/HomeScreen.kt
@@ -1,16 +1,25 @@
 package com.example.memoryexplorer.ui.screens.home
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Gamepad
+import androidx.compose.material.icons.filled.HeartBroken
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,14 +29,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.example.memoryexplorer.data.database.Memory
+import com.example.memoryexplorer.R
 import com.example.memoryexplorer.ui.MemoryExplorerRoute
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    val items = (1..20).map { "Item n°$it" }
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -40,52 +53,85 @@ fun HomeScreen(navController: NavHostController) {
             }
         },
     ) { contentPadding ->
-        Column(
-            modifier = Modifier
-                .padding(contentPadding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(Modifier.size(16.dp))
+        Spacer(Modifier.size(16.dp))
+        if(items.isEmpty()) {
             Text(
                 "Vuoto",
-                color = MaterialTheme.colorScheme.onSecondaryContainer,
-                style = MaterialTheme.typography.bodyMedium,
                 textAlign = TextAlign.Center
             )
+        } else {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 80.dp),
+                modifier = Modifier.padding(contentPadding)
+            ) {
+                items(items) { item ->
+                    MemoryItem(
+                        item,
+                        onClick = {
+                            Log.d("HomeScreen", "Item clicked: $item")
+                        }
+                    )
+                }
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MemoryItem(item: Memory, onClick: () -> Unit) {
-    Card(
-        onClick = onClick,
-        modifier = Modifier
-            .size(150.dp)
-            .fillMaxWidth(),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer
-        )
+fun MemoryItem(item: String, onClick: () -> Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
+        Card(
+            onClick = onClick,
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(Modifier.size(8.dp))
-            item.title?.let {
-                Text(
-                    it,
-                    color = MaterialTheme.colorScheme.onSecondaryContainer,
-                    style = MaterialTheme.typography.bodyMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
+                .fillMaxWidth()
+                .size(160.dp),
+            ) {
+            Image(
+                // TODO add image   painter = painterResource(R.drawable.default_memory),
+                Icons.Default.Gamepad,
+                "Memory picture",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
         }
+        Spacer(Modifier.size(6.dp))
+        Row(
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                item,
+            )
+            Icon(
+                Icons.Default.HeartBroken,
+                contentDescription = "Memory picture",
+                modifier = Modifier.clickable {
+                    Log.d("MemoryItem", "Heart clicked")
+                }
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                "02/02/2002",
+            )
+        }
+        Spacer(Modifier.size(10.dp))
     }
 }
