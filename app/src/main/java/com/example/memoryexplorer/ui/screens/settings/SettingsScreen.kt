a3d9fb722651041a0174306a27e5acd5bd8bdace
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2024-04-23 16:09:04 +0200

    Added dynamic language selector and theme

--- app/src/main/java/com/example/memoryexplorer/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/memoryexplorer/ui/screens/settings/SettingsScreen.kt
@@ -1,35 +1,156 @@
 package com.example.memoryexplorer.ui.screens.settings
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import com.example.memoryexplorer.data.repositories.LoginRepository
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.memoryexplorer.R
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
     navController: NavHostController,
     loginRepository: LoginRepository,
     onLogout: (NavHostController, LoginRepository) -> Unit
 ) {
+    val context = LocalContext.current
+    val theme = arrayOf(R.string.theme_system, R.string.theme_light, R.string.theme_dark)
+        .map { stringResource(it) }
+        .toTypedArray()
+    val language = R.string::class.java.fields
+        .filter { it.name.startsWith("language_") }
+        .mapNotNull { it.getInt(it) }
+        .map { context.getString(it) }
+        .toTypedArray()
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf(theme[0]) }
 
-    ClickableText(
-        text = AnnotatedString(
-            stringResource(R.string.logout),
-            SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold, fontSize = 20.sp)
-        ),
-        onClick = { onLogout(navController, loginRepository) },
-        modifier = Modifier.padding(top = 16.dp, start = 16.dp)
-    )
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp)
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize(),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+            ) {
+                Text(
+                    text = stringResource(R.string.select_theme)
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    ExposedDropdownMenuBox(
+                        expanded = expanded,
+                        onExpandedChange = {
+                            expanded = !expanded
+                        }
+                    ) {
+                        TextField(
+                            value = selectedText,
+                            onValueChange = {},
+                            readOnly = true,
+                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                            modifier = Modifier
+                                .menuAnchor()
+                                .fillMaxWidth()
+                        )
+
+                        ExposedDropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            theme.forEach { item ->
+                                DropdownMenuItem(
+                                    text = { Text(text = item) },
+                                    onClick = {
+                                        selectedText = item
+                                        expanded = false
+                                        Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+            Divider(color = Color.Gray, thickness = 1.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, bottom = 16.dp)
+            ) {
+                Text(text = stringResource(R.string.select_language))
+            }
+            language.forEach { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                ) {
+                    Button(
+                        onClick = { Toast.makeText(context, item, Toast.LENGTH_SHORT).show() },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        // TODO add icon flag
+                        Icon(Icons.Filled.Image, contentDescription = null)
+                        Text(text = item)
+                    }
+                }
+            }
+            Divider(color = Color.Gray, thickness = 1.dp)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Button(
+                    onClick = { onLogout(navController, loginRepository) },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = stringResource(R.string.logout))
+                }
+            }
+        }
+    }
 }
